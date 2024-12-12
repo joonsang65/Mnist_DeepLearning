5015a30826b310e1900ff98f6d57cd4dfb4231bd
joonsang65 <156314474+joonsang65@users.noreply.github.com>
2024-12-12 17:09:08 +0900

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+# 활성화 & 손실함수
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+# 이미지 변환 함수
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+# 다수의 이미지를 입력받아 2차원 배열로 평탄화
+
+# Parameters
+# ----------
+# input_data : 4차원 입력 데이터(이미지 수, 채널 수, 높이, 너비)
+# filter_h : 필터의 높이
+# filter_w : 필터의 너비
+# + stride & pading
+
+# 2차원 배열 col Return
+
+    N, C, H, W = input_data.shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+
+    img = np.pad(input_data, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
+    col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
+    return col
+
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+# im2col과 반대로, 2차원 배열을 입력받아 다수의 이미지 묶음으로 변환
+
+# Parameters
+# ----------
+# col : 2차원 배열(입력 데이터)
+# input_shape : 원래 이미지 데이터의 형상（예：(10, 1, 28, 28)）
+
+# 변환된 이미지 Return
+
+    N, C, H, W = input_shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+    col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2*pad + stride - 1, W + 2*pad + stride - 1))
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]
+
